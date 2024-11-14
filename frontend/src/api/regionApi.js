be6f29acef64f3cc3e3bc842178f7654cd2c1921
burhanusr <bsultanramadan@gmail.com>
2024-11-15 00:44:14 +0700

    add change to region api call

--- frontend/src/api/regionApi.js
+++ frontend/src/api/regionApi.js
@@ -2,28 +2,28 @@ import axios from "axios";
 
 export const getAllProvinces = async () => {
   const response = await axios.get(
-    "/api/api-wilayah-indonesia/api/provinces.json",
+    "https://emsifa.github.io/api-wilayah-indonesia/api/provinces.json",
   );
   return response.data;
 };
 
 export const getAllRegencies = async (provinceId) => {
   const response = await axios.get(
-    `/api/api-wilayah-indonesia/api/regencies/${provinceId}.json`,
+    `https://emsifa.github.io/api-wilayah-indonesia/api/regencies/${provinceId}.json`,
   );
   return response.data;
 };
 
 export const getAllDistricts = async (regencyId) => {
   const response = await axios.get(
-    `/api/api-wilayah-indonesia/api/districts/${regencyId}.json`,
+    `https://emsifa.github.io/api-wilayah-indonesia/api/districts/${regencyId}.json`,
   );
   return response.data;
 };
 
 export const getAllVillages = async (districtId) => {
   const response = await axios.get(
-    `/api/api-wilayah-indonesia/api/villages/${districtId}.json`,
+    `https://emsifa.github.io/api-wilayah-indonesia/api/villages/${districtId}.json`,
   );
   return response.data;
 };
