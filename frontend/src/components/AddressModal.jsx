fa66130c6d4c73c472629dc3e22224b9b282176c
burhanusr <bsultanramadan@gmail.com>
2024-11-14 23:40:59 +0700

    fix wrong path in AddressModal.jsx

--- frontend/src/components/AddressModal.jsx
+++ frontend/src/components/AddressModal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import Button from "./../components/ui/Button/Button";
-import { ComboBox } from "./ui/Combobox";
+import { ComboBox } from "./ui/ComboBox";
 import { addAddress, updateAddress } from "../api/deliveryAddressApi";
 import toast from "react-hot-toast";
 
